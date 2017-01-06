2094fc9beb2895ddf7b9e92eb5e305f34402ae4a
Nigel Burnett <nigelorion@gmail.com>
2017-01-06 12:39:19 -0800

    Add styles

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,8 +1,6 @@
-var apiKey = require('./../.env').apiKey;
+var Search = require('./../js/scripts.js').searchModule;
 
 $(function() {
-
-  console.log(apiKey);
 
   $("form").submit(function(){
     $(".main__results").empty();
@@ -11,25 +9,14 @@ $(function() {
 
     var issue = $("#issue").val();
 
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=30&user_key=' + apiKey).then(function(response){
+    var search = new Search();
 
-      var doctors = response.data;
+    search.searchIssue(issue);
 
-      console.log(doctors);
-
-      doctors.forEach(function(doctor) {
-        var name = doctor.profile.first_name + " " + doctor.profile.last_name;
-        var img = doctor.profile.image_url;
-        var star = doctor.rating.image_url_large_2x ;
-        console.log(star);
-
-        $(".main__results").append("<div class='main__results__doctors'>" + "<h1>" + name + "</h1>" + "<img src=" + img + " />" + "<img src=" + star + " />" + "<div>");
-
-
-      });
-
-
-    });
+    // results.forEach(function(doctor){
+    //   $(".main__results").append(doctor);
+    //
+    // });
 
   });
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,39 @@
+var apiKey = require('./../.env').apiKey;
 
+function Search(issue) {
+  this.issue = issue;
+}
+
+Search.prototype.searchIssue = function (issue) {
+
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=30&user_key=' + apiKey).then(function(response){
+
+    var doctors = response.data;
+
+    console.log(response);
+
+    doctors.forEach(function(doctor) {
+
+      var name = doctor.profile.first_name + " " + doctor.profile.last_name;
+
+      var img = doctor.profile.image_url;
+
+      var specialties = doctor.specialties;
+
+      console.log(specialties);
+
+      specialties.forEach(function(titles){
+        var title = titles.actor;
+
+      });
+
+
+      $(".main__results").append("<div class='main__results__doctors'>" + "<h1>" + name + "</h1>" + "<img src=" + img + " />" + "<div>");
+
+    });
+
+  });
+
+};
+
+exports.searchModule = Search;
