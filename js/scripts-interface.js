882b910902de3e86f05d2635f05e16981710811b
Nigel Burnett <nigelorion@gmail.com>
2017-01-06 09:53:08 -0800

    Add results to Dom

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -5,13 +5,28 @@ $(function() {
   console.log(apiKey);
 
   $("form").submit(function(){
+    $(".main__results").empty();
 
     event.preventDefault();
 
     var issue = $("#issue").val();
 
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=30&user_key=' + apiKey).then(function(response){
-      console.log(response);
+
+      var doctors = response.data;
+
+      console.log(doctors);
+
+      doctors.forEach(function(doctor) {
+        var name = doctor.profile.first_name + " " + doctor.profile.last_name;
+        var img = doctor.profile.image_url;
+        console.log(name);
+
+        $(".main__results").append("<div class='main__results__doctors'>" + "<h1>" + name + "</h1>" + "<img src=" + img + " />" + "<div>");
+
+
+      });
+
 
     });
 
