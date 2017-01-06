0546adf357cad3fccd660a6745f11c665b93ccff
Nigel Burnett <nigelorion@gmail.com>
2017-01-06 10:17:55 -0800

    Add rating images

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -11,7 +11,7 @@ $(function() {
 
     var issue = $("#issue").val();
 
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=30&user_key=' + apiKey).then(function(response){
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + issue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=30&user_key=' + apiKey).then(function(response){
 
       var doctors = response.data;
 
@@ -20,9 +20,10 @@ $(function() {
       doctors.forEach(function(doctor) {
         var name = doctor.profile.first_name + " " + doctor.profile.last_name;
         var img = doctor.profile.image_url;
-        console.log(name);
+        var star = doctor.rating.image_url_large_2x ;
+        console.log(star);
 
-        $(".main__results").append("<div class='main__results__doctors'>" + "<h1>" + name + "</h1>" + "<img src=" + img + " />" + "<div>");
+        $(".main__results").append("<div class='main__results__doctors'>" + "<h1>" + name + "</h1>" + "<img src=" + img + " />" + "<img src=" + star + " />" + "<div>");
 
 
       });
